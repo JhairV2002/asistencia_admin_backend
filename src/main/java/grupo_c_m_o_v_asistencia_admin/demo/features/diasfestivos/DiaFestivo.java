1f2201a1da428ed74b19fa704d839ef9bad081a8
Roberto Obregon <81327803+Fenixx87@users.noreply.github.com>
2022-07-14 11:16:28 -0500

    backend dias festivos

---
+++ src/main/java/grupo_c_m_o_v_asistencia_admin/demo/features/diasfestivos/DiaFestivo.java
@@ -0,0 +1,30 @@
+package grupo_c_m_o_v_asistencia_admin.demo.features.diasfestivos;
+
+import java.sql.Timestamp;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Data;
+
+@Data
+@Table("dias_festivos")
+public class DiaFestivo {
+
+    @Id
+    @Column("id_dia_festivo")
+    private long dayId;
+
+    @Column("nombre_dia_festivo")
+    private String name;
+
+    @Column("fecha_dia_festivo")
+    private Timestamp date;
+
+    private Timestamp created;
+    private Timestamp updated;
+    private boolean archived;
+    private boolean enabled;
+    
+}
---
+++ src/main/java/grupo_c_m_o_v_asistencia_admin/demo/features/diasfestivos/DiaFestivoController.java
@@ -0,0 +1,52 @@
+package grupo_c_m_o_v_asistencia_admin.demo.features.diasfestivos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/diaFestivo")
+@CrossOrigin({"*"})
+public class DiaFestivoController {
+    
+    @Autowired
+    DiaFestivoService diaFestivoService;
+
+    //CRUD
+
+    //Create
+    @PostMapping("/save")
+    public DiaFestivo save(@RequestBody DiaFestivo diaFestivo){
+        return diaFestivoService.save(diaFestivo);
+    }
+
+    //Read
+    @GetMapping("/{id}")
+    public DiaFestivo findById(@PathVariable long id){
+        return diaFestivoService.findById(id);
+    }
+
+    //Update
+    @PutMapping("/update")
+    public DiaFestivo update(@RequestBody DiaFestivo diaFestivo)
+    {
+        return diaFestivoService.save(diaFestivo);
+    }
+
+    //Delete
+    @DeleteMapping("/deleteById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable long id){
+        diaFestivoService.deleteById(id);
+    }
+
+}
