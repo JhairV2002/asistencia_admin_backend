874c860546832d3d92f5b6ddaea2e7dc182454aa
1752035343 <jag.vasquez@yavirac.edu.ec>
2022-07-14 11:22:32 -0500

    crud completo y funcional

---
+++ src/main/java/grupo_c_m_o_v_asistencia_admin/demo/feature/rol/RolRepository.java
@@ -0,0 +1,8 @@
+package grupo_c_m_o_v_asistencia_admin.demo.feature.rol;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RolRepository extends CrudRepository<Rol, Long> {
+
+    
+}
