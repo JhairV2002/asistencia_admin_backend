648573ac12d30c80d54f3582d76843baf6f4b88c
1752035343 <jag.vasquez@yavirac.edu.ec>
2022-07-24 20:46:12 -0500

    roles findAll and findByName

--- src/main/java/grupo_c_m_o_v_asistencia_admin/demo/feature/rol/RolRepository.java
+++ src/main/java/grupo_c_m_o_v_asistencia_admin/demo/feature/rol/RolRepository.java
@@ -1,8 +1,13 @@
 package grupo_c_m_o_v_asistencia_admin.demo.feature.rol;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface RolRepository extends CrudRepository<Rol, Long> {
 
-    
+    List<Rol> findAll();
+
+    List<Rol> findByNameLikeIgnoreCase(String term);
+
 }
--- src/main/java/grupo_c_m_o_v_asistencia_admin/demo/feature/rol/RolService.java
+++ src/main/java/grupo_c_m_o_v_asistencia_admin/demo/feature/rol/RolService.java
@@ -1,5 +1,7 @@
 package grupo_c_m_o_v_asistencia_admin.demo.feature.rol;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,20 +13,28 @@ public class RolService {
     @Autowired
     RolRepository rolRepository;
 
-    public Rol save (Rol rol){
-        return rolRepository.save(rol);
-    } 
-
-    public Rol findById(long id){
-        return rolRepository.findById(id).orElse(new Rol());
-    } 
-
-    public Rol update(Rol rol){
+    public Rol save(Rol rol) {
         return rolRepository.save(rol);
     }
 
-    public void deleteRol (long id){
+    public Rol findById(long id) {
+        return rolRepository.findById(id).orElse(new Rol());
+    }
+
+    public Rol update(Rol rol) {
+        return rolRepository.save(rol);
+    }
+
+    public void deleteRol(long id) {
         rolRepository.deleteById(id);
     }
-    
+
+    public List<Rol> findAll() {
+        return rolRepository.findAll();
+    }
+
+    public List<Rol> findByName(String term) {
+        return rolRepository.findByNameLikeIgnoreCase(term);
+    }
+
 }
